b7460eb533d1ed773dff1d799901211e97454e33
Arik Sosman <git@arik.io>
2020-01-23 11:44:33 -0800

    use better variable names

--- src/hkdf.ts
+++ src/hkdf.ts
@@ -3,18 +3,18 @@ import * as crypto from 'crypto';
 export default class HKDF {
 
 	static derive(salt: Buffer, master: Buffer) {
-		const prk = crypto.createHmac('sha256', salt)
+		const key = crypto.createHmac('sha256', salt)
 			.update(master)
 			.digest();
 
-		const t1 = crypto.createHmac('sha256', prk)
+		const part1 = crypto.createHmac('sha256', key)
 			.update(Buffer.from([1]))
 			.digest();
-		const t2 = crypto.createHmac('sha256', prk)
-			.update(t1)
+		const part2 = crypto.createHmac('sha256', key)
+			.update(part1)
 			.update(Buffer.from([2]))
 			.digest();
 
-		return Buffer.concat([t1, t2]);
+		return Buffer.concat([part1, part2]);
 	}
 }
