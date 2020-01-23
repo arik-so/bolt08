cf2bd7b274fb871a835519ad0df6837d6a66718d
Arik Sosman <git@arik.io>
2020-01-23 11:41:10 -0800

    simplify HKDF

--- src/hkdf.ts
+++ src/hkdf.ts
@@ -2,26 +2,19 @@ import * as crypto from 'crypto';
 
 export default class HKDF {
 
-	private static readonly HASH_LENGTH = 32;
+	static derive(salt: Buffer, master: Buffer) {
+		const prk = crypto.createHmac('sha256', salt)
+			.update(master)
+			.digest();
 
-	static derive(salt: Buffer, master: Buffer, length: number = 64, info: string = ''): Buffer {
-		// RFC5869 Step 1 (Extract)
-		const prk = crypto.createHmac('sha256', salt).update(master).digest();
+		const t1 = crypto.createHmac('sha256', prk)
+			.update(Buffer.from([1]))
+			.digest();
+		const t2 = crypto.createHmac('sha256', prk)
+			.update(t1)
+			.update(Buffer.from([2]))
+			.digest();
 
-		const hashLength = this.HASH_LENGTH;
-
-		// RFC5869 Step 2 (Expand)
-		let t = new Uint8Array([]);
-		const okm = new Uint8Array(Math.ceil(length / hashLength) * hashLength);
-		const uintInfo = Buffer.from(info, 'ascii');
-		for (let i = 0; i < Math.ceil(length / hashLength); i++) {
-			const concat = new Uint8Array(t.length + uintInfo.length + 1);
-			concat.set(t);
-			concat.set(uintInfo, t.length);
-			concat.set(new Uint8Array([i + 1]), t.length + uintInfo.length);
-			t = crypto.createHmac('sha256', prk).update(concat).digest();
-			okm.set(t, hashLength * i);
-		}
-		return Buffer.from(okm.slice(0, length));
+		return Buffer.concat([t1, t2]);
 	}
 }
